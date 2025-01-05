8c2cc1017f6cd65a0089dbc2441aba977459e159
mcr <700215+mcr85@users.noreply.github.com>
2025-01-05 15:25:41 +0100

    feat: s04E03 - answering all the questions, sending report and successfully getting flag

--- S04E03/app.ts
+++ S04E03/app.ts
@@ -1,14 +1,7 @@
-/**
- *  1. Zapamiętuj w dowolnej formie linki już odwiedzone, aby uniknąć zapętlenia mechanizmu
-    2. Na stronie mogą pojawić się 2-3 linki, które potencjalnie wyglądają, jakby mogły zawierać wartościowe dane. Nie wchodź w każdy z nich, a jedynie w ten najbardziej prawdopodobny.
-    3. Możesz (ale nie musisz!) sporządzić mapę odwiedzonych stron i ich zawartości. Szukając odpowiedzi na pytanie nr 01, prawdopodobnie odwiedzisz kilka stron. Czy szukając odpowiedzi na pytanie 02 i 03 naprawdę musisz odwiedzać je ponownie i znów wysyłać ich treść do LLM-a?
- */
 import chalk from 'chalk';
-import { readFile, writeFile } from 'fs/promises';
 import FirecrawlApp, { type ScrapeResponse } from '@mendable/firecrawl-js';
 import { OpenAIService } from '../mcr_lib/OpenAIService';
 import { VectorService } from '../mcr_lib/VectorService';
-import path from 'path';
 
 const apiKey = process.env.CENTRALA;
 const fireCrawlApiKey = process.env.FIRECRAWL_API_KEY;
@@ -29,25 +22,10 @@ interface Page {
     links: Link[];
 }
 
-// const pagesFile: { pages: Page[] } = await import('./pages.json');
-
 const mainPageUrl = 'https://softo.ag3nts.org/';
-const contactPageUrl = 'https://softo.ag3nts.org/kontakt';
-const portfolioBanan = 'https://softo.ag3nts.org/portfolio_1_c4ca4238a0b923820dcc509a6f75849b';
 const questionsUrl = `https://centrala.ag3nts.org/data/${apiKey}/softo.json`
 
-// TODO: get main section links
-// discovered links won't have descriptions/summary yet. You can always updated data about them later
-
 async function getPage(url: string): Promise<Page> {
-    // const pagesFile: { pages: Page[] } = await import('./pages.json');
-    // const page = pagesFile.pages.find((page) => page.url === url);
-
-    // if (page) {
-    //     console.log(chalk.yellow('📚 Got page from cache', page));
-    //     return page;
-    // }
-
     console.log(chalk.green('🔍 Scraping page url:'), url);
     const firecrawl = new FirecrawlApp({ apiKey: fireCrawlApiKey });
     const result = await firecrawl.scrapeUrl(url, {
@@ -61,21 +39,6 @@ async function getPage(url: string): Promise<Page> {
     const text = result.markdown;
     const links = extractLinks(result.markdown);
 
-    // TODO: add summary?
-
-    // TODO: add embedding
-
-    // pagesFile.pages.push({
-    //     url,
-    //     title,
-    //     description,
-    //     content,
-    //     links
-    // });
-
-    // TODO: write to qdrant instead
-    // await writeFile(path.join(__dirname, 'pages.json'), JSON.stringify(pagesFile, null, 2));
-
     return {
         url,
         title,
@@ -85,11 +48,20 @@ async function getPage(url: string): Promise<Page> {
     } as Page;
 }
 
+async function addPageToDatabase(vectorService: VectorService, page: Page) {
+    const metadata = { ...page };
+    delete metadata.text;
+
+    return vectorService.addPoints(COLLECTION_NAME, [{
+        text: page.text ?? '',
+        metadata
+    }]);
+}
+
 function extractLinks(text: string) {
     const baseUrl = 'https://softo.ag3nts.org';
     const links = [];
 
-    // const re = /\[(.+)\]\((\/\w+)\s"(.+)"/g
     const re = /\[(.*?)\]\((.*?)\s?(?:"(.*?)")?\)/gm
     let match;
 
@@ -104,13 +76,13 @@ function extractLinks(text: string) {
 
         links.push(link);
 
-        console.log(chalk.blueBright('🔗 Got link', link))
+        // console.log(chalk.blueBright(`🔗 Got link ${link.url}`));
     }
 
     return links;
 }
 async function getQuestions() {
-    console.log(chalk.green(' Fetching questions...'));
+    console.log(chalk.green('Fetching questions...'));
 
     let questions: Record<string, string>;
 
@@ -153,49 +125,10 @@ Return answer in following json format:
         }
     )
         .then((response) => {
-            return JSON.parse(response);
+            const answerData = JSON.parse(response);
+            console.log(chalk.greenBright('🎉 Got answer to question:'), answerData.answer);
+            return answerData;
         });
-
-
-    // TODO: add openai chat completion for above prompt
-
-    // ask OpenAI for to extract keywords of what is being asked, like email address, company name, etc.
-    // first try to search by keywords in database (vector db?)
-    // if can't find it in database, then search on the web
-
-    // 1. get most probable page
-    // plan: get page where you can find the answer and find it
-
-    // extract links with firecrawl: url, title, keywords, description
-    // const page = await getPage(mainPageUrl);
-    // const page = await getPage(contactPageUrl);
-    // const page = await getPage(portfolioBanan);
-
-    // console.log('page:', page);
-
-        // .then((result) => {
-        //     const url = result.metadata?.url;
-        //     const title = result.metadata?.title;
-        //     const description = result.metadata?.description;
-        //     const content = result.markdown;
-        //     const links = extractLinks(result.markdown);
-        //     console.log('result:', result);
-        //     console.log('result markdown', result.markdown);
-        //     console.log('result links', links);
-
-        //     return {
-        //         url,
-        //         title,
-        //         description,
-        //         content,
-        //         links
-        //     } as Page;
-
-        //     // links:
-        //     // url,
-        //     // title,
-        //     // description,
-        // });
 }
 
 async function pickMostProbableLink(openAIService: OpenAIService, links: Link[], question: string) {
@@ -215,7 +148,6 @@ Return most probable link in the same json format as provided above json:
 "title": title
 }`;
 
-// console.log('mcpl prompt', prompt);
     return openAIService.send(
         question,
         [
@@ -231,138 +163,88 @@ Return most probable link in the same json format as provided above json:
         }
     )
         .then((response) => {
+            console.log(chalk.yellow('Most probable link:\n'), response);
             return JSON.parse(response);
         });
 }
 
+async function sendAnswers(payload: unknown): Promise<Response> {
+    const body = {
+        task: 'softo',
+        apikey: apiKey,
+        answer: payload
+    };
 
-// loop through questions
-// determine page to get from database
+    console.log('mcr payload', payload);
 
-// check in qdrant if you can find a page for given question
-
-// if no candidate, determine page to get from web
-// check if answer is in page content
+    return fetch('https://centrala.ag3nts.org/report', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+    });
+}
 
 async function main() {
     const openAIService = new OpenAIService();
     const vectorService = new VectorService(openAIService);
-
     await vectorService.ensureCollection(COLLECTION_NAME);
 
     const questions = await getQuestions();
+    const answers: Record<string, string> = {};
+    // const question = questions['03'];
 
-    const question = questions['03'];
     let link = mainPageUrl;
-    let answer;
 
     const pagesChecked = new Set();
 
-    // TODO: potential answer
-    while (!answer) {
-        // TODO: this needs to take new link/page
-        const searchResults = await vectorService.performSearch(COLLECTION_NAME, question, {}, 1);
-        let page;
+    // go through all the questions
+    for (const questionNr of Object.keys(questions)) {
+        const question = questions[questionNr];
+        let answer;
 
-        console.log('Got page from database, search results:', searchResults);
-
-        // GET page either from database or from web
-        if (searchResults!.length > 0 && searchResults[0].score > 0.5) {
-            console.log('got page for question from database');
-            page = searchResults[0].payload;
-        }
-        else {
-            console.log('no page for question in database');
-
-            // while (!answer) {
-                // getting page
+        while (!answer) {
+            const searchResults = await vectorService.performSearch(COLLECTION_NAME, question, {}, 1);
+            let page;
+    
+            if (searchResults!.length > 0 && searchResults[0].score > 0.5) {
+                page = searchResults[0].payload;
+                console.log('Got page for question from database, url:', page.url);
+            }
+            else {
+                console.log('No page for question in database or low score.');
                 page = await getPage(link);
+                await addPageToDatabase(vectorService, page);
+            }
 
-                // adding it to db
-                const metadata = { ...page };
-                delete metadata.text;
-
-                vectorService.addPoints(COLLECTION_NAME, [{
-                    text: page.text ?? '',
-                    metadata
-                }]);
-
-                // page = {
-                //     text: page.text,
-                //     ...metadata
-                // } as Page;
-
-            // }
-        }
-
-        // GET answer from page
-        // console.log('page links', page.links);
-        const answerResponse = await getAnswer(openAIService, question, page.text);
-
-        console.log('mcr answerResponse', answerResponse);
-
-        if (!answerResponse.answer || answerResponse.answer === 'false') {
-            // TODO: no answer, set new link to go through and advance loop
-            pagesChecked.add(page.url);
-            console.log('No answer found in page content. Searching for most probable link...');
-            const links = page.links.filter((link) => !pagesChecked.has(link.url));
-            console.log('Links to check:', links);
-            const mostProbableLink = await pickMostProbableLink(openAIService, links, question);
-            console.log('most probable link:', mostProbableLink);
-            link = mostProbableLink.url;
-
-            console.log('mcr debug getting page link', link, typeof mostProbableLink);
-            page = await getPage(link);
-
-            // adding it to db
-            const metadata = { ...page };
-            delete metadata.text;
-
-            vectorService.addPoints(COLLECTION_NAME, [{
-                text: page.text ?? '',
-                metadata
-            }]);
-
-            // TODO: scrape page and add to database
-        } else {
-            // TODO: got answer, break the loop
-            answer = answerResponse.answer;
-            console.log(chalk.greenBright('🎉 Got answer to question:', answer));
+            if (!page) {
+                throw new Error('No page found for question in database or on the web. Url: ' + link);
+            }
+    
+            const answerResponse = await getAnswer(openAIService, question, page.text);
+    
+            if (!answerResponse.answer || answerResponse.answer === 'false') {
+                pagesChecked.add(page.url);
+                console.log('No answer found in page content. Searching for most probable link...');
+                const links = page.links.filter((link) => !pagesChecked.has(link.url));
+                const mostProbableLink = await pickMostProbableLink(openAIService, links, question);
+                link = mostProbableLink.url;
+                page = await getPage(link);
+                await addPageToDatabase(vectorService, page);
+            } else {
+                answer = answerResponse.answer;
+                answers[questionNr] = answer;
+            }
         }
     }
 
-    // const answer = await getAnswer(questions['01']);
+    console.log('Answers:', answers);
+
+    const centralaResponse = await sendAnswers(answers);
+    const centralaResponseJson = await centralaResponse.json(); 
+
+    console.log('Centrala response:', centralaResponseJson);
 }
-
-
-// console.log('mcr pagesFile', pagesFile.pages);
-
-
-
-
-/**
-You're a robot which gets some questions
-You need to search for answers on SoftoAI website
-Your entry point is `pageMainUrl`. Scrape it for content and links. Store content and page metadata in visitedPages array in this format:
-{
-    url: pageUrl,
-    content: pageContent,
-    summary: pageSummary
-    links: array of pageLinks, where pageLink: { url: linkUrl, keywords: string[], title: string, description: string }
-}
-
-Store data about page
-
-Try to find answer in page content. If you can't find it, follow the most probable link and repeat the process.
-
-*/
-
-
-// question 01: "Podaj adres mailowy do firmy SoftoAI"
-// 
-
-
-// console.log('questions:', questions);
-// console.log('answers:', '01', answer);
 
 main();
